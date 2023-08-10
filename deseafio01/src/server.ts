7f275d7d5774d8d3da18b7b8bdaa714108f5e690
Bryan <bryan.b.valeriano@gmail.com>
2023-08-10 15:57:57 -0300

    add middleware to create Json body for the request

--- deseafio01/src/server.ts
+++ deseafio01/src/server.ts
@@ -1,6 +1,14 @@
 import http from 'node:http'
-const server = http.createServer((req, res) => {
-  return res.end('Hello 8')
+import { json } from './middlewares/json';
+import { IincomingMessage } from './definitions';
+
+const server = http.createServer(async (req: IincomingMessage, res) => {
+  //const { method, url } = req;
+
+  await json(req, res);
+  console.log(req.body)
+
+  return res.end('Hello 8');
 })
 
 server.listen(3333);
