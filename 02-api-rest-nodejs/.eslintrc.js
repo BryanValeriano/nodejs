c62f66ff9e6a01ce91c583efc00dae79316c3c85
BryanValeriano <bryan.b.valeriano@gmail.com>
2023-08-22 17:39:00 -0300

    fix eslint

--- 02-api-rest-nodejs/.eslintrc.js
+++ 02-api-rest-nodejs/.eslintrc.js
@@ -1,6 +1,8 @@
 module.exports = {
-    parser: '@typescript-eslint/parser',
+  parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
-  extends: [],
+  extends: [
+    "airbnb"
+  ],
   rules: {}
 }
