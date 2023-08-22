ab385eed81de726140d61949117fbabdbf278a78
BryanValeriano <bryan.b.valeriano@gmail.com>
2023-08-22 19:00:45 -0300

    add knex connection to database

--- 02-api-rest-nodejs/.eslintrc.js
+++ 02-api-rest-nodejs/.eslintrc.js
@@ -2,7 +2,27 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   extends: [
-    "airbnb"
+    'airbnb',
   ],
-  rules: {}
-}
+  rules: {
+    'import/prefer-default-export': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        extensions: ['.js', '.ts', '.d.ts', '.tsx'],
+      },
+    },
+  },
+};
