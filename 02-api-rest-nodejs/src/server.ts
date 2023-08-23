a5f3d5c623d0617c3913ce57ceb751ba0befe9cb
BryanValeriano <bryan.b.valeriano@gmail.com>
2023-08-23 19:13:39 -0300

    add example db query to server

--- 02-api-rest-nodejs/src/server.ts
+++ 02-api-rest-nodejs/src/server.ts
@@ -1,11 +1,16 @@
 import fastify from 'fastify';
+import crypto from 'node:crypto';
 import { knex } from './database';
 
 const app = fastify();
 
 app.get('/hello', async () => {
-  const tables = knex('sqlite_schema').select('*');
-  return tables;
+  const transaction = await knex('transactions').insert({
+    id: crypto.randomUUID(),
+    title: 'Transacao teste',
+    ammount: 1000,
+  }).returning('*');
+  return transaction;
 });
 
 app.listen({
