6a76a4d6cdd68af99715718826e19ce7944e0b33
BryanValeriano <bryan.b.valeriano@gmail.com>
2023-08-26 15:21:03 -0300

    add getAll and getSingle transactions to /transactions route

--- 02-api-rest-nodejs/src/routes/transactions.ts
+++ 02-api-rest-nodejs/src/routes/transactions.ts
@@ -4,19 +4,37 @@ import { randomUUID } from 'crypto';
 import { knex } from '../database';
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select();
+    return { transactions };
+  });
+
+  app.get('/:id', async (request) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    });
+
+    const { id } = getTransactionParamsSchema.parse(request.params);
+
+    const transaction = await knex('transactions').where('id', id).first();
+
+    return { transaction };
+  });
+
   app.post('/', async (request, reply) => {
-    const createTransactionBodySchema = z.object({
+    const getTransactionBodySchema = z.object({
       title: z.string(),
       amount: z.number(),
       type: z.enum(['credit', 'debit']),
     });
 
-    const { title, amount, type } = createTransactionBodySchema.parse(request.body);
+    const { title, amount, type } = getTransactionBodySchema.parse(request.body);
 
     await knex('transactions').insert({
       id: randomUUID(),
       title,
       ammount: type === 'credit' ? amount : amount * -1,
+
     });
 
     return reply.status(201).send();
