c0c5002015a63204ba90d179dc3b1833226b06dd
BryanValeriano <bryan.b.valeriano@gmail.com>
2023-08-26 16:16:19 -0300

    add hook example

--- 02-api-rest-nodejs/src/server.ts
+++ 02-api-rest-nodejs/src/server.ts
@@ -6,6 +6,9 @@ import { transactionsRoutes } from './routes/transactions';
 const app = fastify();
 
 app.register(cookie);
+app.addHook('preHandler', async (request) => {
+  console.log(`[${request.method}] ${request.url}`);
+});
 app.register(transactionsRoutes, {
   prefix: 'transactions',
 });
