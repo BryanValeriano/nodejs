87350e83045a07a0404d2339d5f70211122f205a
BryanValeriano <bryan.b.valeriano@gmail.com>
2023-08-30 01:39:23 -0300

    add knex to project

--- desafio02-DailyDiet/src/server.ts
+++ desafio02-DailyDiet/src/server.ts
@@ -1,8 +1,12 @@
 import fastify from 'fastify';
+import { knex } from './database';
 
 const app = fastify();
 
-app.get('/hello', () => 'Hello World!');
+app.get('/hello', async () => {
+  const test = await knex('sqlite_schema').select('*');
+  return test;
+});
 
 app.listen({
   port: 3333,
