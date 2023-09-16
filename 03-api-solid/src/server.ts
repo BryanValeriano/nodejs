46decd7fcc0802a64947dfeb46cee654b95bbeea
BryanValeriano <bryan.b.valeriano@gmail.com>
2023-09-16 17:03:37 -0300

    fix eslint && add import alias

--- 03-api-solid/src/server.ts
+++ 03-api-solid/src/server.ts
@@ -1,12 +1,12 @@
-import { app } from './app'
-import { env } from './env'
+import { app } from './app';
+import { env } from '@/env';
 
 app.listen({
   host: '0.0.0.0',
   port: env.PORT
 }).then(() => {
-  console.log('HTTP Server Running!')
+  console.log('HTTP Server Running!');
 }).catch((err) => {
-  console.log('Error on starting server')
-  throw new Error(err)
-})
+  console.log('Error on starting server');
+  throw new Error(err);
+});
