92982274bd87235feafabf2b5d3a12fcf5876a7c
BryanValeriano <bryan.b.valeriano@gmail.com>
2023-10-06 02:45:11 -0300

    add unit test to userAlreadyExistsError

--- 03-api-solid/src/use-cases/registerUseCase.spec.ts
+++ 03-api-solid/src/use-cases/registerUseCase.spec.ts
@@ -2,6 +2,7 @@ import { describe, expect, it } from 'vitest';
 import { RegisterUseCase } from './registerUseCase';
 import { compare } from 'bcryptjs';
 import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository';
+import { UserAlreadyExistsError } from './errors/user-already-exists-error';
 
 describe('Register Use Case', () => {
   it('Shoud hash user password upon registration', async () => {
@@ -21,5 +22,24 @@ describe('Register Use Case', () => {
     );
 
     expect(isPasswordCorrectlyHashed).toBe(true);
+  });
+
+  it('Shoud not be able to register with same email twice', async () => {
+    const repository = new InMemoryUsersRepository();
+    const registerUseCase = new RegisterUseCase(repository);
+
+    const password = 'passwordSecret';
+    await registerUseCase.execute({
+      name: 'John Doe',
+      email: 'johnDoe@gmail.com',
+      password
+    });
+
+    expect(() => registerUseCase.execute({
+      name: 'John Doe',
+      email: 'johnDoe@gmail.com',
+      password
+    })
+    ).rejects.toBeInstanceOf(UserAlreadyExistsError);
   });
 });
