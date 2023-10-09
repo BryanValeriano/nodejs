33fba9bf9030ca765281a06533e967a17063bf7a
BryanValeriano <bryan.b.valeriano@gmail.com>
2023-10-08 21:17:54 -0300

    add authenticateUser use case

---
+++ 03-api-solid/src/use-cases/authenticateUserUseCase.ts
@@ -0,0 +1,37 @@
+import { IUsersRepository } from '@/repositories/IUsersRepository';
+import { User } from '@prisma/client';
+import { InvalidCredentialsError } from './errors/invalid-credentials-error';
+import { compare } from 'bcryptjs';
+
+interface AuthenticateUserUseCaseRequest {
+  email: string,
+  password: string
+}
+
+interface AuthenticateUserUseCaseResponse {
+  user: User
+}
+
+export class AuthenticateUserUseCase {
+  constructor(private usersRepository: IUsersRepository) { }
+
+  async execute({
+    email,
+    password
+  }: AuthenticateUserUseCaseRequest): Promise<AuthenticateUserUseCaseResponse> {
+
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new InvalidCredentialsError();
+    }
+
+    const doesPasswordMatches = await compare(password, user.password_hash);
+
+    if (!doesPasswordMatches) {
+      throw new InvalidCredentialsError();
+    }
+
+    return { user };
+  }
+}
