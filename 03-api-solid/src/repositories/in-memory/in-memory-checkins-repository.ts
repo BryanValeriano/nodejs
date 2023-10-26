431798dc8cc5e578b16c35974703680bc1a2fccb
BryanValeriano <bryan.b.valeriano@gmail.com>
2023-10-25 22:43:37 -0300

    create check ins repository

---
+++ 03-api-solid/src/repositories/in-memory/in-memory-checkins-repository.ts
@@ -0,0 +1,19 @@
+import { Prisma, CheckIn } from '@prisma/client';
+import { ICheckInsRepository } from '../ICheckInsRepository';
+import { randomUUID } from 'node:crypto';
+
+export class InMemoryCheckInsRepository implements ICheckInsRepository {
+  public checkIns: CheckIn[] = [];
+
+  async create(data: Prisma.CheckInUncheckedCreateInput) {
+    const checkIn = {
+      id: randomUUID(),
+      user_id: data.user_id,
+      gym_id: data.gym_id,
+      validated_at: data.validated_at ? new Date(data.validated_at) : null,
+      created_at: new Date()
+    };
+    await this.checkIns.push(checkIn);
+    return checkIn;
+  }
+}
